3b6b1b41cf7dc7d2c7054dd5fde5ed29e5418b01
xyyx <xyyx@mail.ru>
2018-08-20 16:33:58 +0800

    PixysSettings: Use switch for devices without variable button brightness
    Change-Id: Id5c9b6492fad77045561f75202cf9f872cf7d047
    Signed-off-by: Subinsmani <subins.mani@gmail.com>

--- src/com/pixys/settings/fragments/NavbarSettings.java
+++ src/com/pixys/settings/fragments/NavbarSettings.java
@@ -46,7 +46,16 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.Utils;
 
+import com.pixys.settings.preferences.SystemSettingSeekBarPreference;
+import com.pixys.settings.preferences.SystemSettingSwitchPreference;
+
 public class NavbarSettings extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String KEY_BUTTON_BRIGHTNESS = "button_brightness";
+    private static final String KEY_BUTTON_BRIGHTNESS_SW = "button_brightness_sw";
+
+    private SystemSettingSeekBarPreference mButtonBrightness;
+    private SystemSettingSwitchPreference mButtonBrightness_sw;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -56,6 +65,31 @@ public class NavbarSettings extends SettingsPreferenceFragment implements OnPref
 
         final ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefSet = getPreferenceScreen();
+
+        final boolean variableBrightness = getResources().getBoolean(
+                com.android.internal.R.bool.config_deviceHasVariableButtonBrightness);
+
+        mButtonBrightness =
+                (SystemSettingSeekBarPreference) findPreference(KEY_BUTTON_BRIGHTNESS);
+        mButtonBrightness_sw =
+                (SystemSettingSwitchPreference) findPreference(KEY_BUTTON_BRIGHTNESS_SW);
+
+        if (variableBrightness) {
+            prefScreen.removePreference(mButtonBrightness_sw);
+            if (mButtonBrightness != null) {
+                int ButtonBrightness = Settings.System.getInt(getContentResolver(),
+                        Settings.System.BUTTON_BRIGHTNESS, 255);
+                mButtonBrightness.setValue(ButtonBrightness / 1);
+                mButtonBrightness.setOnPreferenceChangeListener(this);
+            }
+        } else {
+            prefScreen.removePreference(mButtonBrightness);
+            if (mButtonBrightness_sw != null) {
+                mButtonBrightness_sw.setChecked((Settings.System.getInt(getContentResolver(),
+                        Settings.System.BUTTON_BRIGHTNESS, 1) == 1));
+                mButtonBrightness_sw.setOnPreferenceChangeListener(this);
+            }
+        }
 
     }
 
@@ -69,8 +103,19 @@ public class NavbarSettings extends SettingsPreferenceFragment implements OnPref
         super.onResume();
     }
 
-    @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mButtonBrightness) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.BUTTON_BRIGHTNESS, value * 1);
+            return true;
+        } else if (preference == mButtonBrightness_sw) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.BUTTON_BRIGHTNESS, value ? 1 : 0);
+            return true;
+        }
         return false;
     }
 }
