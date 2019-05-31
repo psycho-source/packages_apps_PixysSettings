e4078f81bebf65697832e99521de0dc014a6dbb1
Henrique Silva <jhenrique09.mcz@hotmail.com>
2019-05-31 14:07:49 -0300

    [2/2] NetworkTraffic: Rework
    * Keep only up/down mode for better UX
    
    Change-Id: I8fdf1592ac7e620da469c14087e4be0c10d5990d

--- src/com/pixys/settings/fragments/Traffic.java
+++ src/com/pixys/settings/fragments/Traffic.java
@@ -47,7 +47,7 @@ public class Traffic extends SettingsPreferenceFragment implements OnPreferenceC
     private static final String NETWORK_TRAFFIC_CATEGORY = "network_traffic";
 
     private PreferenceCategory mNetworkTrafficCategory;
-    private DropDownPreference mNetTrafficMode;
+    private DropDownPreference mNetTrafficEnabled;
     private SwitchPreference mNetTrafficAutohide;
     private DropDownPreference mNetTrafficUnits;
     private SwitchPreference mNetTrafficShowUnits;
@@ -65,12 +65,12 @@ public class Traffic extends SettingsPreferenceFragment implements OnPreferenceC
         if (!isNetworkTrafficAvailable()) {
             getPreferenceScreen().removePreference(mNetworkTrafficCategory);
         }else{
-            mNetTrafficMode = (DropDownPreference)
-                    findPreference(Settings.System.NETWORK_TRAFFIC_MODE);
-            mNetTrafficMode.setOnPreferenceChangeListener(this);
+            mNetTrafficEnabled = (DropDownPreference)
+                    findPreference(Settings.System.NETWORK_TRAFFIC_ENABLED);
+            mNetTrafficEnabled.setOnPreferenceChangeListener(this);
             int mode = Settings.System.getIntForUser(resolver,
-                    Settings.System.NETWORK_TRAFFIC_MODE, 0, UserHandle.USER_CURRENT);
-            mNetTrafficMode.setValue(String.valueOf(mode));
+                    Settings.System.NETWORK_TRAFFIC_ENABLED, 0, UserHandle.USER_CURRENT);
+            mNetTrafficEnabled.setValue(String.valueOf(mode));
 
             mNetTrafficAutohide = (SwitchPreference)
                     findPreference(Settings.System.NETWORK_TRAFFIC_AUTOHIDE);
@@ -112,10 +112,10 @@ public class Traffic extends SettingsPreferenceFragment implements OnPreferenceC
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mNetTrafficMode) {
+        if (preference == mNetTrafficEnabled) {
             int mode = Integer.valueOf((String) newValue);
             Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.NETWORK_TRAFFIC_MODE, mode);
+                    Settings.System.NETWORK_TRAFFIC_ENABLED, mode);
             updateNetworkTrafficEnabledStates(mode);
         } else if (preference == mNetTrafficUnits) {
             int units = Integer.valueOf((String) newValue);
