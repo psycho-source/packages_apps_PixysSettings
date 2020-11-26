de484f2f66f0c510241353cb8b283ec8c839a22b
Psycho Mods <nishsin26@gmail.com>
2020-11-26 15:32:53 +0530

    Update the UI for PixysSettigns

--- src/com/pixys/settings/PixysSettings.java
+++ src/com/pixys/settings/PixysSettings.java
@@ -17,6 +17,8 @@
 
 package com.pixys.settings;
 
+import com.android.internal.logging.nano.MetricsProto;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -24,16 +26,84 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
 
-public class PixysSettings extends Fragment {
+public class PixysSettings extends SettingsPreferenceFragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.pixys_settings_layout, container, false);
+        getActivity().setTitle('Explore');
+
+        FragmentManager fragmentManager = getChildFragmentManager();
+
+        RelativeLayout statusbar_settings = view.findViewById(R.id.pixys_statusbar);
+        RelativeLayout quick_settings = view.findViewById(R.id.pixys_quicksettings);
+        RelativeLayout notification_settings = view.findViewById(R.id.pixys_notification);
+        RelativeLayout buttons_settings = view.findViewById(R.id.pixys_buttons);
+        RelativeLayout lockscreen_settings = view.findViewById(R.id.pixys_lockscreen);
+        RelativeLayout gesture_settings = view.findViewById(R.id.pixys_gestures);
+        RelativeLayout misc_settings = view.findViewById(R.id.pixys_miscellaneous);
+
+        /*statusbar_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction().replace(R.id.content, new PixysStatusbarSettings()).commit();
+            }
+        });
+
+        quick_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction().replace(R.id.content, new PixysStatusbarSettings()).commit();
+            }
+        });
+
+        notification_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction().replace(R.id.content, new PixysStatusbarSettings()).commit();
+            }
+        });
+
+        buttons_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction().replace(R.id.content, new PixysStatusbarSettings()).commit();
+            }
+        });
+
+        lockscreen_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction().replace(R.id.content, new PixysStatusbarSettings()).commit();
+            }
+        });
+
+        gesture_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction().replace(R.id.content, new PixysStatusbarSettings()).commit();
+            }
+        });
+
+        misc_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentManager.beginTransaction().replace(R.id.content, new PixysStatusbarSettings()).commit();
+            }
+        });*/
+
         return view;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.PIXYS;
     }
 
 }
