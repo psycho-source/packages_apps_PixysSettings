c1d8ed03f05ec51d80e2ba958a9bd2ba45fff99d
Psycho Mods <nishsin26@gmail.com>
2020-11-29 19:39:16 +0530

    Fix back button behaviour in Extra Settings
    Change-Id: I77971eb7509cb142a5aa340b656d1cfb0ea16691

--- src/com/pixys/settings/PixysSettings.java
+++ src/com/pixys/settings/PixysSettings.java
@@ -24,6 +24,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,8 +35,10 @@ import com.android.settings.SettingsPreferenceFragment;
 
 import com.pixys.settings.fragments.PixysExploreFragment;
 
-public class PixysSettings extends SettingsPreferenceFragment implements PixysPreferenceScreenChangeListener {
+public class PixysSettings extends SettingsPreferenceFragment implements PixysPreferenceScreenChangeListener, View.OnKeyListener {
 
+    static long frag_id = 0;
+    
     FragmentManager fragmentManager;
 
     @Override
@@ -46,9 +49,17 @@ public class PixysSettings extends SettingsPreferenceFragment implements PixysPr
 
         fragmentManager = getChildFragmentManager();
 
-        fragmentManager.beginTransaction().replace(R.id.content, new PixysExploreFragment()).commit();
+        fragmentManager.beginTransaction().add(R.id.content, new PixysExploreFragment(), String.valueOf(frag_id)).commit();
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+        view.setOnKeyListener(this);
     }
 
     @Override
@@ -58,7 +69,23 @@ public class PixysSettings extends SettingsPreferenceFragment implements PixysPr
 
     @Override
     public void onPixysPreferenceScreenChange(Fragment fragment) {
-        fragmentManager.beginTransaction().replace(R.id.content, fragment).addToBackStack(null).commit();
+        frag_id++;
+        fragmentManager.beginTransaction().setCustomAnimations(R.anim.pixys_settings_exit_anim, R.anim.pixys_settings_enter_anim, R.anim.pixys_settings_exit_anim, R.anim.pixys_settings_enter_anim).replace(R.id.content, fragment, String.valueOf(frag_id)).addToBackStack(String.valueOf(frag_id)).commit();
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_UP) {
+            if (keyCode == KeyEvent.KEYCODE_BACK) {
+                if (fragmentManager.getBackStackEntryCount() > 0) {
+                    if(fragmentManager.getBackStackEntryCount() == 1)
+                        getActivity().setTitle("Explore");
+                    fragmentManager.popBackStack();
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
 }
